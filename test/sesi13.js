42678d2ec128d291cbe4b5574b0f966e3ffc9636
Binti Amaliyah <bintiamaliyah9@gmail.com>
2025-07-04 19:18:49 +0700

    ini sesi13

---
+++ test/sesi13.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai' ;
+
+//nama test suites
+describe ('API Demo Test', () => {
+
+    //nama test case
+    it('coba test aplikasi', async () => {
+        console.log('Aplikasi terbuka');
+    });
+});
